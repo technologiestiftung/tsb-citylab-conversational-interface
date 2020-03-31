73a7dec93dbaae8600b00b5c82aa966b2aa966c2
Dennis Ostendorf <dennisostendorf@web.de>
2020-03-31 16:56:12 +0200

    fix: include person identifier in aggregate function

--- src/components/PDF/aggregateResponses.js
+++ src/components/PDF/aggregateResponses.js
@@ -1,46 +1,53 @@
-const requiredResponses = [
-  'r-first-name',
-  'r-last-name',
-  'r-birth-name',
-  'r-other-names',
-  'r-doctor',
-  'r-sex',
-  'r-birth-date',
-  'r-birth-place',
-  'r-religion',
-  'r-nationality',
-];
-
-const aggregatePersonInfo = person => {
-  console.assert(
-    requiredResponses.every(item => Object.keys(person).includes(item)),
-    'Received responses do not correspond with required responses'
-  );
-
+const aggregatePerson = (data, personID) => {
   return {
     name: {
-      first: person['r-first-name'].value,
+      first: data[`r-${personID}-first-name`].value,
       last:
-        person['r-doctor'].value === 1
-          ? `Dr. ${person['r-last-name'].value}`
-          : person['r-last-name'].value,
+        data[`r-${personID}-doctor`].value === 1
+          ? `Dr. ${data[`r-${personID}-last-name`].value}`
+          : data[`r-${personID}-last-name`].value,
       birth:
-        person['r-birth-name'].value === 0
-          ? `${person['r-first-name'].value} ${person['r-last-name'].value}`
-          : person['r-birth-name-manual'].value,
+        data[`r-${personID}-birth-name`].value === 0
+          ? `${data[`r-${personID}-first-name`].value} ${
+              data[`r-${personID}-last-name`].value
+            }`
+          : data[`r-${personID}-birth-name-manual`].value,
       other:
-        person['r-other-names'].value === 0
+        data[`r-${personID}-other-names-manual`].value === 0
           ? '-'
-          : person['r-other-names-manual'].value,
+          : data[`r-${personID}-other-names-manual`].value,
     },
-    sex: person['r-sex'].value,
-    birth: `${person['r-birth-date'].value}, ${person['r-birth-place'].value}`,
+    sex: data[`r-${personID}-sex`].value,
+    birth: `${data[`r-${personID}-birth-date`].value}, ${
+      data[`r-${personID}-birth-place`].value
+    }`,
     religion:
-      person['r-religion'].value === 0
+      data[`r-${personID}-religion`].value === 0
         ? 'keine'
-        : person['r-religion-manual'].value,
-    nationality: person['r-nationality'].value,
+        : data[`r-${personID}-religion-manual`].value,
+    nationality: data[`r-${personID}-nationality`].value,
   };
 };
 
-export default aggregatePersonInfo;
+const aggregateResponses = responses => {
+  return {
+    flats: {
+      new: {},
+      previous: {},
+      others: {},
+    },
+    persons: {
+      first: {
+        info: aggregatePerson(responses, 'p1'),
+        docs: {},
+      },
+      second: {
+        info: {},
+        docs: {},
+      },
+      maritalStatus: {},
+    },
+  };
+};
+
+export default aggregateResponses;
