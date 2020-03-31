9c1df63b9732fbb539ec945cbddac45d6899d280
Dennis Ostendorf <dennisostendorf@web.de>
2020-03-31 16:59:53 +0200

    feature: enable PDF modification with new data

--- src/components/PDF/index.js
+++ src/components/PDF/index.js
@@ -1,22 +1,24 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
-import download from 'downloadjs'
+import download from 'downloadjs';
 
 import Button from '~/components/Button';
-import aggregatePersonInfo from './aggregateResponses';
+import aggregateResponses from './aggregateResponses';
 
 // const anmeldungPDFLink = "https://www.berlin.de/formularverzeichnis/?formular=/labo/zentrale-einwohnerangelegenheiten/_assets/anmeldung_bei_der_meldebehoerde.pdf"
-const anmeldungPDF = "/public/pdf/anmeldung_bei_der_meldebehoerde.pdf"
+const anmeldungPDF = '/public/pdf/anmeldung_bei_der_meldebehoerde.pdf';
 // const anmeldungPDF = "/public/pdf/test.pdf"
 
 const CreatePDF = p => {
   const { steps } = p;
 
-  const person1 = aggregatePersonInfo(steps);
+  const responseData = aggregateResponses(steps);
 
-  const modifyPDF = async person => {
-    const existingPdfBytes = await fetch(anmeldungPDF).then(res => res.arrayBuffer());
+  const modifyPDF = async responses => {
+    const existingPdfBytes = await fetch(anmeldungPDF).then(res =>
+      res.arrayBuffer()
+    );
     const pdfDoc = await PDFDocument.load(existingPdfBytes);
 
     const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
@@ -24,25 +26,91 @@ const CreatePDF = p => {
     const pages = pdfDoc.getPages();
     const firstPage = pages[0];
 
-    const { width, height } = firstPage.getSize();
+    const person1 = responses.persons.first.info;
 
-    firstPage.drawText(person.name.last, {
+    firstPage.drawText(person1.name.last, {
       x: 150,
       y: 575,
       size: 16,
       font: helveticaFont,
-      color: rgb(0, 0, 0)
-    })
+      color: rgb(0, 0, 0),
+    });
+
+    firstPage.drawText(person1.name.first, {
+      x: 150,
+      y: 545,
+      size: 11,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+
+    firstPage.drawText(person1.name.birth, {
+      x: 150,
+      y: 527,
+      size: 11,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+
+    firstPage.drawText(person1.name.other, {
+      x: 150,
+      y: 456,
+      size: 11,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+
+    firstPage.drawText(person1.sex, {
+      x: 150,
+      y: 512,
+      size: 11,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+
+    firstPage.drawText(person1.birth, {
+      x: 150,
+      y: 498,
+      size: 11,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+
+    firstPage.drawText(person1.religion, {
+      x: 150,
+      y: 484,
+      size: 11,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
+
+    firstPage.drawText(person1.nationality, {
+      x: 150,
+      y: 470,
+      size: 11,
+      font: helveticaFont,
+      color: rgb(0, 0, 0),
+    });
 
     const pdfBytes = await pdfDoc.save()
-    download(pdfBytes, "anmeldung_bei_der_meldebehoerde.pdf", "application/pdf");
+    download(
+      pdfBytes,
+      'anmeldung_bei_der_meldebehoerde.pdf',
+      'application/pdf'
+    );
   }
 
   return (
     <div>
-      <Button onClick={() => {modifyPDF(person1)}}>Formular downloaden</Button>
+      <Button
+        onClick={() => {
+          modifyPDF(responseData);
+        }}
+      >
+        Formular downloaden
+      </Button>
     </div>
-  )
-}
+  );
+};
 
 export default CreatePDF;
