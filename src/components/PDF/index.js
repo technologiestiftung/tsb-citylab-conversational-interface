1499c364ed60801d10650554443ee91c3a15dc21
Dennis Ostendorf <dennisostendorf@web.de>
2020-04-07 16:15:22 +0200

    enable PDF modification by iteration

--- src/components/PDF/index.js
+++ src/components/PDF/index.js
@@ -4,7 +4,7 @@ import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 import download from 'downloadjs';
 
 import Button from '~/components/Button';
-import aggregateResponses from './aggregateResponses';
+import createPersonFields from './createFields/persons';
 
 // const anmeldungPDFLink = "https://www.berlin.de/formularverzeichnis/?formular=/labo/zentrale-einwohnerangelegenheiten/_assets/anmeldung_bei_der_meldebehoerde.pdf"
 const anmeldungPDF = '/public/pdf/anmeldung_bei_der_meldebehoerde.pdf';
@@ -13,9 +13,11 @@ const anmeldungPDF = '/public/pdf/anmeldung_bei_der_meldebehoerde.pdf';
 const CreatePDF = p => {
   const { steps } = p;
 
-  const responseData = aggregateResponses(steps);
+  const person1 = createPersonFields(steps, 'p1');
 
-  const modifyPDF = async responses => {
+  const aggregatedFields = { ...person1 };
+
+  const modifyPDF = async fields => {
     const existingPdfBytes = await fetch(anmeldungPDF).then(res =>
       res.arrayBuffer()
     );
@@ -26,70 +28,16 @@ const CreatePDF = p => {
     const pages = pdfDoc.getPages();
     const firstPage = pages[0];
 
-    const person1 = responses.persons.first.info;
+    const fieldsArray = Object.values(fields);
 
-    firstPage.drawText(person1.name.last, {
-      x: 150,
-      y: 575,
-      size: 16,
-      font: helveticaFont,
-      color: rgb(0, 0, 0),
-    });
-
-    firstPage.drawText(person1.name.first, {
-      x: 150,
-      y: 545,
-      size: 11,
-      font: helveticaFont,
-      color: rgb(0, 0, 0),
-    });
-
-    firstPage.drawText(person1.name.birth, {
-      x: 150,
-      y: 527,
-      size: 11,
-      font: helveticaFont,
-      color: rgb(0, 0, 0),
-    });
-
-    firstPage.drawText(person1.name.other, {
-      x: 150,
-      y: 456,
-      size: 11,
-      font: helveticaFont,
-      color: rgb(0, 0, 0),
-    });
-
-    firstPage.drawText(person1.sex, {
-      x: 150,
-      y: 512,
-      size: 11,
-      font: helveticaFont,
-      color: rgb(0, 0, 0),
-    });
-
-    firstPage.drawText(person1.birth, {
-      x: 150,
-      y: 498,
-      size: 11,
-      font: helveticaFont,
-      color: rgb(0, 0, 0),
-    });
-
-    firstPage.drawText(person1.religion, {
-      x: 150,
-      y: 484,
-      size: 11,
-      font: helveticaFont,
-      color: rgb(0, 0, 0),
-    });
-
-    firstPage.drawText(person1.nationality, {
-      x: 150,
-      y: 470,
-      size: 11,
-      font: helveticaFont,
-      color: rgb(0, 0, 0),
+    fieldsArray.forEach(field => {
+      firstPage.drawText(field.text, {
+        x: field.x,
+        y: field.y,
+        size: 11,
+        font: helveticaFont,
+        color: rgb(0, 0, 0),
+      });
     });
 
     const pdfBytes = await pdfDoc.save()
@@ -104,7 +52,7 @@ const CreatePDF = p => {
     <div>
       <Button
         onClick={() => {
-          modifyPDF(responseData);
+          modifyPDF(aggregatedFields);
         }}
       >
         Formular downloaden
