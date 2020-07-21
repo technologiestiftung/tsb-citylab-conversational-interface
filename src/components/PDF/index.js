0f334f3f39363898524f6b9a284cbc9e400b115e
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-07-21 13:36:37 +0200

    feat(cra): move to create-react-app

--- src/components/PDF/index.js
+++ src/components/PDF/index.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 import download from 'downloadjs'
 
-import Button from '~/components/Button';
+import Button from '../Button';
 
 // const anmeldungPDFLink = "https://www.berlin.de/formularverzeichnis/?formular=/labo/zentrale-einwohnerangelegenheiten/_assets/anmeldung_bei_der_meldebehoerde.pdf"
-const anmeldungPDF = "/public/pdf/anmeldung_bei_der_meldebehoerde.pdf"
+const anmeldungPDF = "/pdf/anmeldung_bei_der_meldebehoerde.pdf"
 // const anmeldungPDF = "/public/pdf/test.pdf"
 
 const CreatePDF = p => {
