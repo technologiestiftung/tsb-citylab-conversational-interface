3a89a3cc249a0b34fa2c66be87ddf8b2ee3e4571
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-07-21 14:36:09 +0200

    test: add basic test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import defaultTheme from './style/themes/default';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders title in header', () => {
+  const { getByText } = render(
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+    </ThemeProvider>
+  );
+  const titleElement = getByText(/CityLAB Chatbot/i);
+  expect(titleElement).toBeInTheDocument();
 });
